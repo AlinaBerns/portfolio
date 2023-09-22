5d2478b287a3e1b9f3620416dc17862e1bc41d38
AlinaBerns <123542234+AlinaBerns@users.noreply.github.com>
2023-09-22 14:02:28 +0200

    +routerlink

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
+import { ProjectsComponent } from './projects/projects.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'about', component: AboutComponent },
+  { path: 'projects', component: ProjectsComponent},
+  { path: 'contact', component: ContactComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
@@ -6,14 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent implements OnInit{
+
+  
+  
   ImagePathAbout!: string;
   ImagePathContact!: string;
   ImagePathProject!: string;
+  
   constructor(){
     this.ImagePathContact = '/assets/google_mail_icon_131657.png';
     this.ImagePathAbout = '/assets/google_docs_icon_131691.png'
     this.ImagePathProject = '/assets/google_keep_icon_131684.png'
+  
   }
+
   ngOnInit(): void {
     throw new Error('Method not implemented.');
   }
