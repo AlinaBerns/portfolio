047494559e98b6cb016a24b29597e1ce44d30770
AlinaBerns <123542234+AlinaBerns@users.noreply.github.com>
2023-09-22 14:45:19 +0200

    ik moet nog werken met scrolling

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
-export class AboutComponent {
+export class AboutComponent implements OnInit {
+  ImagePerson!: string;
 
+  constructor(){
+    this.ImagePerson='/assets/2023-09-21_18-00-45.png';
+  }
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
 }
